27b52f12cfa705e19a26283249c049f78f8bb3cc
罗海强ever <1452677304@qq.com>
2022-10-11 10:17:22 +0800

    jkjkn

---
+++ ADCoreArc/ArcSoftFace/ADCoreSystem/Camera/ImageModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcSoftFace.ADCoreSystem
+{
+    public class ImageModel
+    {
+        /// <summary>
+        /// 图片路径
+        /// </summary>
+        public List<string> ImagePath { get; set; }
+        /// <summary>
+        /// 图片
+        /// </summary>
+        public List<Image> images { get; set; }    
+    }
+}
---
+++ ADCoreArc/ArcSoftFace/GameCommon/FaceRegister/FaceDataMode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcSoftFace.GameCommon 
+{
+    public  class FaceDataMode
+    {
+        public  string  Name { get; set; }  
+        public string GroupID { get; set; } 
+        public  Byte[]  FaceData { get; set; }
+    }
+}
