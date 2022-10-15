bba20c180e6fcec5044d0f41fb04c55553f4f1c6
罗海强ever <1452677304@qq.com>
2022-10-15 16:30:20 +0800

    liojij

---
+++ ADCoreArc/ArcSoftFace/ADCoreSystem/ADCoreGameWindow/ServerSettingWindow.Designer.cs
@@ -0,0 +1,235 @@
+﻿
+namespace ArcSoftFace.ADCoreSystem.ADCoreGameWindow
+{
+    partial class ServerSettingWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.uiLabel1 = new Sunny.UI.UILabel();
+            this.uiLabel2 = new Sunny.UI.UILabel();
+            this.ServerSettingNameInpiut = new Sunny.UI.UITextBox();
+            this.uiLabel3 = new Sunny.UI.UILabel();
+            this.PortInput = new Sunny.UI.UITextBox();
+            this.uiLabel4 = new Sunny.UI.UILabel();
+            this.AccountInput = new Sunny.UI.UITextBox();
+            this.uiLabel5 = new Sunny.UI.UILabel();
+            this.PasswordInput = new Sunny.UI.UITextBox();
+            this.SureBtn = new Sunny.UI.UIButton();
+            this.BackBtn = new Sunny.UI.UIButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(112, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(556, 407);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // uiLabel1
+            // 
+            this.uiLabel1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiLabel1.Location = new System.Drawing.Point(135, 41);
+            this.uiLabel1.Name = "uiLabel1";
+            this.uiLabel1.Size = new System.Drawing.Size(121, 23);
+            this.uiLabel1.TabIndex = 1;
+            this.uiLabel1.Text = "服务器设置";
+            this.uiLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiLabel1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiLabel2
+            // 
+            this.uiLabel2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiLabel2.Location = new System.Drawing.Point(157, 102);
+            this.uiLabel2.Name = "uiLabel2";
+            this.uiLabel2.Size = new System.Drawing.Size(120, 23);
+            this.uiLabel2.TabIndex = 2;
+            this.uiLabel2.Text = "服务器设置：";
+            this.uiLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiLabel2.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // ServerSettingNameInpiut
+            // 
+            this.ServerSettingNameInpiut.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.ServerSettingNameInpiut.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ServerSettingNameInpiut.Location = new System.Drawing.Point(284, 102);
+            this.ServerSettingNameInpiut.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ServerSettingNameInpiut.MinimumSize = new System.Drawing.Size(1, 16);
+            this.ServerSettingNameInpiut.Name = "ServerSettingNameInpiut";
+            this.ServerSettingNameInpiut.ShowText = false;
+            this.ServerSettingNameInpiut.Size = new System.Drawing.Size(335, 29);
+            this.ServerSettingNameInpiut.TabIndex = 3;
+            this.ServerSettingNameInpiut.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ServerSettingNameInpiut.Watermark = "";
+            this.ServerSettingNameInpiut.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiLabel3
+            // 
+            this.uiLabel3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiLabel3.Location = new System.Drawing.Point(187, 153);
+            this.uiLabel3.Name = "uiLabel3";
+            this.uiLabel3.Size = new System.Drawing.Size(69, 23);
+            this.uiLabel3.TabIndex = 4;
+            this.uiLabel3.Text = "端口：";
+            this.uiLabel3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiLabel3.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // PortInput
+            // 
+            this.PortInput.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.PortInput.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.PortInput.Location = new System.Drawing.Point(284, 146);
+            this.PortInput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.PortInput.MinimumSize = new System.Drawing.Size(1, 16);
+            this.PortInput.Name = "PortInput";
+            this.PortInput.ShowText = false;
+            this.PortInput.Size = new System.Drawing.Size(335, 29);
+            this.PortInput.TabIndex = 5;
+            this.PortInput.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.PortInput.Watermark = "";
+            this.PortInput.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiLabel4
+            // 
+            this.uiLabel4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiLabel4.Location = new System.Drawing.Point(191, 208);
+            this.uiLabel4.Name = "uiLabel4";
+            this.uiLabel4.Size = new System.Drawing.Size(73, 23);
+            this.uiLabel4.TabIndex = 6;
+            this.uiLabel4.Text = "账号：";
+            this.uiLabel4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiLabel4.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // AccountInput
+            // 
+            this.AccountInput.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.AccountInput.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.AccountInput.Location = new System.Drawing.Point(284, 208);
+            this.AccountInput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.AccountInput.MinimumSize = new System.Drawing.Size(1, 16);
+            this.AccountInput.Name = "AccountInput";
+            this.AccountInput.ShowText = false;
+            this.AccountInput.Size = new System.Drawing.Size(335, 29);
+            this.AccountInput.TabIndex = 7;
+            this.AccountInput.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.AccountInput.Watermark = "";
+            this.AccountInput.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiLabel5
+            // 
+            this.uiLabel5.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiLabel5.Location = new System.Drawing.Point(195, 260);
+            this.uiLabel5.Name = "uiLabel5";
+            this.uiLabel5.Size = new System.Drawing.Size(82, 23);
+            this.uiLabel5.TabIndex = 8;
+            this.uiLabel5.Text = "密码：";
+            this.uiLabel5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiLabel5.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // PasswordInput
+            // 
+            this.PasswordInput.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.PasswordInput.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.PasswordInput.Location = new System.Drawing.Point(284, 260);
+            this.PasswordInput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.PasswordInput.MinimumSize = new System.Drawing.Size(1, 16);
+            this.PasswordInput.Name = "PasswordInput";
+            this.PasswordInput.ShowText = false;
+            this.PasswordInput.Size = new System.Drawing.Size(335, 29);
+            this.PasswordInput.TabIndex = 9;
+            this.PasswordInput.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.PasswordInput.Watermark = "";
+            this.PasswordInput.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // SureBtn
+            // 
+            this.SureBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SureBtn.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.SureBtn.Location = new System.Drawing.Point(284, 339);
+            this.SureBtn.MinimumSize = new System.Drawing.Size(1, 1);
+            this.SureBtn.Name = "SureBtn";
+            this.SureBtn.Size = new System.Drawing.Size(100, 35);
+            this.SureBtn.TabIndex = 10;
+            this.SureBtn.Text = "确定";
+            this.SureBtn.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.SureBtn.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // BackBtn
+            // 
+            this.BackBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BackBtn.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.BackBtn.Location = new System.Drawing.Point(519, 339);
+            this.BackBtn.MinimumSize = new System.Drawing.Size(1, 1);
+            this.BackBtn.Name = "BackBtn";
+            this.BackBtn.Size = new System.Drawing.Size(100, 35);
+            this.BackBtn.TabIndex = 11;
+            this.BackBtn.Text = "取消";
+            this.BackBtn.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.BackBtn.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // ServerSettingWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 450);
+            this.Controls.Add(this.BackBtn);
+            this.Controls.Add(this.SureBtn);
+            this.Controls.Add(this.PasswordInput);
+            this.Controls.Add(this.uiLabel5);
+            this.Controls.Add(this.AccountInput);
+            this.Controls.Add(this.uiLabel4);
+            this.Controls.Add(this.PortInput);
+            this.Controls.Add(this.uiLabel3);
+            this.Controls.Add(this.ServerSettingNameInpiut);
+            this.Controls.Add(this.uiLabel2);
+            this.Controls.Add(this.uiLabel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "ServerSettingWindow";
+            this.Text = "ServerSettingWindow";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Sunny.UI.UILabel uiLabel1;
+        private Sunny.UI.UILabel uiLabel2;
+        private Sunny.UI.UITextBox ServerSettingNameInpiut;
+        private Sunny.UI.UILabel uiLabel3;
+        private Sunny.UI.UITextBox PortInput;
+        private Sunny.UI.UILabel uiLabel4;
+        private Sunny.UI.UITextBox AccountInput;
+        private Sunny.UI.UILabel uiLabel5;
+        private Sunny.UI.UITextBox PasswordInput;
+        private Sunny.UI.UIButton SureBtn;
+        private Sunny.UI.UIButton BackBtn;
+    }
+}
